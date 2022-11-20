b36f5a998400873fd14bb780d9a67e35af86af8e
Denis <lost.ned@gmail.com>
2022-11-20 19:20:02 +0300

    Added ifElse

---
+++ Example006_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "liza")
+{
+    Console.WriteLine("Ура, это любимая!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
